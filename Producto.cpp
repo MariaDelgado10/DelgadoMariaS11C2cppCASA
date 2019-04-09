b488cbf5dcdf7308313fa6ae4f4db1475736b544
MariaDelgado10 <49251994+MariaDelgado10@users.noreply.github.com>
2019-04-09 12:39:44 -0500

    Producto de los dos arreglos y producto punto

--- Producto.cpp
+++ Producto.cpp
@@ -2,14 +2,14 @@
 #include<iostream>
 
 int producto( int[],  int[],int tamano);
-int productopunto(int[],int[]);
+int productopunto(int[],int[],int tamano);
 
 int main()
     {
          int arreglo1[5]={ 1,2,3,4,5 } ;
          int arreglo2[5]={10,20,30,40,50 };
     std::cout<<"el producto de los arreglos es "<< producto(arreglo1,arreglo2,5);
-    //std::cout<<"el producto punto de los dos arreglos es "<< productopunto(9);
+    std::cout<<"el producto punto de los dos arreglos es "<< productopunto(arreglo1,arreglo2,5);
     
     }
     
@@ -31,6 +31,26 @@ int producto( int arreglo1[],  int arreglo2[], int tamano)
     }
     
     return producto[5];
+//sd::cout<<"El factorial de "<< n <<"es"<<fac<<std::endl;
+    }
+int productopunto( int arreglo1[],  int arreglo2[], int tamano)
+   
+    {
+        
+    int productopunto=0;
+    
+    for (int i =1; i<=arreglo1[0]; i++)
+    {
+        for (int j=1; j<=arreglo2[0]; j++)
+        
+        {
+           productopunto+=arreglo1[i]*arreglo2[j];
+
+        }
+        
+    }
+    
+    return productopunto;
 //sd::cout<<"El factorial de "<< n <<"es"<<fac<<std::endl;
     }
     
